fcb6d9e4a0897071a206c983fea097ffea9f11cf
Sanjana Jain <sanjana.jain04@gmail.com>
2020-11-16 21:52:57 +0530

    Added-Method for Cheapest hotel by Hotel by Weekdays and Weekends

--- main/java/HotelReservationMain.java
+++ main/java/HotelReservationMain.java
@@ -7,10 +7,19 @@ public class HotelReservationMain {
         System.out.println("Welcome to Hotel Reservation System");
     }
 
-    public Hotel findCheapestHotel(String startDate, String endDate) {
+    public Hotel findCheapestHotelByWeekdayRates(String startDate, String endDate) {
         Optional<Hotel> cheapestHotel = hotelList.stream()
                 .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
                 .reduce((hotel1, hotel2) -> hotel1.weekdayRate < hotel2.weekdayRate ? hotel1 : hotel2);
+        if (cheapestHotel.isPresent())
+            return cheapestHotel.get();
+        return null;
+    }
+
+    public Hotel findCheapestHotelByWeekendRates(String startDate, String endDate) {
+        Optional<Hotel> cheapestHotel = hotelList.stream()
+                .filter(h -> h.startDate.compareTo(startDate) > 0 && h.endDate.compareTo(endDate) < 0)
+                .reduce((hotel1, hotel2) -> hotel1.weekendRate < hotel2.weekendRate ? hotel1 : hotel2);
         if (cheapestHotel.isPresent())
             return cheapestHotel.get();
         return null;
